0f780f5143d7dd7d6921e50c86a6e4ca2b4559c3
zhao.feng <zhao.feng@Ctrip.com>
2018-06-13 20:19:12 +0800

    first commit

---
+++ 基础部分.playground/Contents.swift
@@ -0,0 +1,34 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//常量
+let maximumNumberOfLoginAttempts = 10
+//变量
+var currentLoginAttempt = 10
+//类型标注
+var welcomeMessage: String = "hello"
+
+//输出
+print("pp\(welcomeMessage)")
+
+//整数
+let intType = 1000_1000
+print(intType)
+
+//元祖
+let tupe = (404,"ERROR")
+print(tupe.0)
+print(tupe.1)
+let (status,_) = tupe
+print("status \(status)")
+let tupe1 = (state:408,msg:"NEW ERROR")
+print(tupe1.msg)
+
+//可选类型
+
+
+
+
+
+
