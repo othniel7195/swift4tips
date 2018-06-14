93a455cc6c1c108445e5cfdfc9023ca004faa17e
wjszf <478043385@qq.com>
2018-06-15 01:12:47 +0800

    字符串和字符

--- 基本运算符.playground/Contents.swift
+++ 基本运算符.playground/Contents.swift
@@ -24,3 +24,49 @@ print("x1:\(x1)")
 //x1 = x1 + 1  \\报错
 x1 = x1 &+ 1
 
+//求余
+9 % 4
+9 % -4
+-9 % 4
+-9 % -4
+
+//比较运算符
+//注意： Swift 也提供恒等（===）和不恒等（!==）这两个比较符来判断两个对象是否引用同一个对象实例
+//注意：Swift 标准库只能比较七个以内元素的元组比较函数。如果你的元组元素超过七个时，你需要自己实现比较运算符。
+
+
+//空合运算符  a != nil ? a! : b
+let defaultColorName = "red"
+var userDefinedColorName: String?   //默认值为 nil
+var colorNameToUse = userDefinedColorName ?? defaultColorName
+// userDefinedColorName 的值为空，所以 colorNameToUse 的值为 "red"
+
+userDefinedColorName = "green"
+colorNameToUse = userDefinedColorName ?? defaultColorName
+// userDefinedColorName 非空，因此 colorNameToUse 的值为 "green"
+
+//闭区间运算符
+for index in 1...5 {
+    print("\(index)")
+}
+print("\n")
+//半开区间运算符
+for index in 1..<5 {
+    print("\(index)")
+}
+print("\n")
+//单侧区间
+let names = ["Anna", "Alex", "Brian", "Jack"]
+for name in names[2...] {
+    print(name)
+}
+print("\n")
+for name in names[..<1] {
+    print(name)
+}
+
+let range = ...5
+range.contains(7)   // false
+range.contains(4)   // true
+range.contains(-100000000)  // true
+print(range)
