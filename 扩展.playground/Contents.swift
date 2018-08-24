a93b52fe6e7ca8bdd021037a9c5cca00affbd528
zhao.feng <zhao.feng@Ctrip.com>
2018-08-24 19:29:54 +0800

    ==扩展

---
+++ 扩展.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//扩展语法
+extension SomeType {
+    // 为 SomeType 添加的新功能写到这里
+}
+
+//注意
+//如果你通过扩展为一个已有类型添加新功能，那么新功能对该类型的所有已有实例都是可用的，即使它们是在这个扩展定义之前创建的。
